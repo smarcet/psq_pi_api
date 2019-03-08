7a6b34fce98cceae20888226a82c0543a53ce2a6
Sebastian Marcet <smarcet@gmail.com>
2019-03-08 18:06:23 -0300

    [smarcet] * added Firebase Dynamic Links Short Links   for stream share

--- api/views/exams.py
+++ api/views/exams.py
@@ -13,6 +13,8 @@ import urllib.parse
 import backend.settings as settings
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import requests
+import json
 
 
 class ExamGenerateShareUrlView(GenericAPIView):
@@ -40,18 +42,40 @@ class ExamGenerateShareUrlView(GenericAPIView):
             h = hmac.new(bytearray(secret, 'utf-8'), str_to_sign.encode('utf-8'), hashlib.sha1)
             digest = h.digest()
             signature = urllib.parse.quote_plus(base64.b64encode(digest))
+            long_url = "{host}/guest/stream?expires={expires}&signature={signature}&device_id={" \
+                       "device_id}&exercise_id={exercise_id}&user_id={user_id}&exercise_max_duration={" \
+                       "exercise_max_duration}".format(
+                host=settings.WEB_HOST,
+                expires=expires,
+                signature=signature,
+                device_id=device_id,
+                exercise_id=exercise_id,
+                exercise_max_duration=exercise_max_duration,
+                user_id=user_id
+            )
+
+            post_url = 'https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key={}'.format(
+                settings.FIREBASE_WEB_API_KEY)
+            # logger.info(post_url)
+            payload = {
+                "dynamicLinkInfo": {
+                    "domainUriPrefix": settings.FIREBASE_DYN_LINK_DOMAIN_PREFIX,
+                    "link": long_url
+                },
+                "suffix": {
+                    "option": "UNGUESSABLE"
+                }
+            }
+
+            headers = {'content-type': 'application/json'}
+            r = requests.post(post_url, data=json.dumps(payload), headers=headers)
+            json_response = r.json()
+
+            if r.status_code != status.HTTP_200_OK:
+                return Response(json_response, status=r.status_code)
 
             return Response({
-                'url': "{host}/guest/stream?expires={expires}&signature={signature}&device_id="
-                       "{device_id}&exercise_id={exercise_id}&user_id={user_id}&exercise_max_duration={exercise_max_duration}".format(
-                    host=settings.WEB_HOST,
-                    expires=expires,
-                    signature=signature,
-                    device_id=device_id,
-                    exercise_id=exercise_id,
-                    exercise_max_duration=exercise_max_duration,
-                    user_id=user_id
-                )
+                'url': json_response["shortLink"]
             }, status=status.HTTP_200_OK)
 
         except:
